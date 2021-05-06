3bc8453d933eeb806a70e554de35d64f753b8506
r079322 - Erwin Van Moorleghem <r0793221@student.thomasmore.be>
2021-05-06 16:07:46 +0200

    Protected Fillable bij aangemaakt voor GSM

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,8 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card col-sm-12">
-                    <div class="bg-transparent  color_Orange font-weight-bolder text-center border-bottom border-left border-right">{{ __('Welkom op My Expense Registratie!') }}</div>
+                    <div
+                        class="bg-transparent  color_Orange font-weight-bolder text-center border-bottom border-left border-right">{{ __('Welkom op My Expense Registratie!') }}</div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
@@ -32,7 +33,8 @@
                                         <input id="firstname" type="text"
                                                class="form-control @error('firstname') is-invalid @enderror"
                                                name="firstname"
-                                               value="{{ old('firstname') }}" required autocomplete="name" autofocus>
+                                               value="{{ old('firstname') }}" required autocomplete="firstname"
+                                               autofocus>
                                         @error('firstname')
                                         <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -48,7 +50,7 @@
                                     <div class="">
                                         <input id="street" type="text"
                                                class="form-control @error('street') is-invalid @enderror"
-                                               name="street" value="{{ old('street') }}" required autocomplete="name"
+                                               name="street" value="{{ old('street') }}" required autocomplete="street"
                                                autofocus>
                                         @error('street')
                                         <span class="invalid-feedback" role="alert">
@@ -64,7 +66,7 @@
                                         <input id="number" type="text"
                                                class="form-control @error('number') is-invalid @enderror"
                                                name="number"
-                                               value="{{ old('number') }}" required autocomplete="name" autofocus>
+                                               value="{{ old('number') }}" required autocomplete="number" autofocus>
                                         @error('number')
                                         <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -74,13 +76,13 @@
                                 </div>
                                 <div class="col-5">
                                     <label for="gsm"
-                                           class="col-form-label text-md-right">{{ __('GSM') }}</label>
+                                           class="col-form-label text-md-right">{{ __('GSM Nummer') }}</label>
                                     <div class="">
                                         <input id="gsm" type="text"
                                                class="form-control @error('gsm') is-invalid @enderror"
                                                name="gsm"
-                                               value="{{ old('gsm') }}" required autocomplete="name" autofocus>
-                                        @error('number')
+                                               value="{{ old('gsm') }}" required autofocus>
+                                        @error('gsm')
                                         <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -95,7 +97,7 @@
                                     <div class="">
                                         <input id="city" type="text"
                                                class="form-control @error('city') is-invalid @enderror"
-                                               name="city" value="{{ old('city') }}" required autocomplete="name"
+                                               name="city" value="{{ old('city') }}" required autocomplete="city"
                                                autofocus>
                                         @error('city')
                                         <span class="invalid-feedback" role="alert">
@@ -111,7 +113,7 @@
                                         <input id="email" type="text"
                                                class="form-control @error('email') is-invalid @enderror"
                                                name="email"
-                                               value="{{ old('email') }}" required autocomplete="name" autofocus>
+                                               value="{{ old('email') }}" required autocomplete="email" autofocus>
                                         @error('email')
                                         <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -143,7 +145,6 @@
                                     <div class="">
                                         <input id="password-confirm" type="password" class="form-control"
                                                name="password_confirmation" required autocomplete="new-password">
-
                                     </div>
                                 </div>
                             </div>
