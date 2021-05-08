88b541100da3f6133196acd20104516e10780836
r079322 - Erwin Van Moorleghem <r0793221@student.thomasmore.be>
2021-05-08 14:39:55 +0200

    'EVM_smallUpdatesInTemplates

--- resources/views/shared/navigation.blade.php
+++ resources/views/shared/navigation.blade.php
@@ -1,22 +1,22 @@
 <nav>
-    <ul class="nav flex-column">
+    <ul class="nav">
         <li class="nav-item">
             <img src="/assets/logo/MyExpenseLogo.png" class="logo" alt="My expense Logo">
         </li>
-        @guest()
-            <li>
-                <a href="/login">
-                    <i class="fas fa-sign-in-alt fa navAws"></i>
-                    <span class="nav-text">Login</span>
-                </a>
-            </li>
-            <li>
-                <a href="/register">
-                    <i class="fas fa-user-plus fa navAws"></i>
-                    <span class="nav-text">Registreer</span>
-                </a>
-            </li>
-        @endguest
+{{--        @guest()--}}
+{{--            <li>--}}
+{{--                <a href="/login">--}}
+{{--                    <i class="fas fa-sign-in-alt fa navAws"></i>--}}
+{{--                    <span class="nav-text">Login</span>--}}
+{{--                </a>--}}
+{{--            </li>--}}
+{{--            <li>--}}
+{{--                <a href="/register">--}}
+{{--                    <i class="fas fa-user-plus fa navAws"></i>--}}
+{{--                    <span class="nav-text">Registreer</span>--}}
+{{--                </a>--}}
+{{--            </li>--}}
+{{--        @endguest--}}
         @Auth
             @if(Auth::user()->active)
                 @include('shared.navigationPartials.active')
@@ -42,57 +42,3 @@
         @endauth
     </ul>
 </nav>
-
-
-
-{{--<nav class="nav flex-column">--}}
-{{--    <div class="menu-icon">--}}
-{{--        <i class="fa fa-bars fa-2x"></i>--}}
-{{--    </div>--}}
-{{--    <div>--}}
-{{--        <img src="/assets/logo/MyExpenseLogo.png" class="logo" alt="My expense Logo">--}}
-{{--    </div>--}}
-{{--    <div class="menu">--}}
-{{--        <ul>--}}
-{{--            @guest()--}}
-{{--                <li>--}}
-{{--                    <a href="/login">--}}
-{{--                        <i class="fas fa-sign-in-alt fa navAws"></i>--}}
-{{--                        <span class="nav-text">Login</span>--}}
-{{--                    </a>--}}
-{{--                </li>--}}
-{{--                <li>--}}
-{{--                    <a href="/register">--}}
-{{--                        <i class="fas fa-user-plus fa navAws"></i>--}}
-{{--                        <span class="nav-text">Registreer</span>--}}
-{{--                    </a>--}}
-{{--                </li>--}}
-{{--            @endguest--}}
-{{--            @Auth--}}
-{{--                @if(Auth::user()->active)--}}
-{{--                    @include('shared.navigationPartials.active')--}}
-{{--                @endif--}}
-
-{{--                @if(Auth::user()->approver)--}}
-{{--                    @include('shared.navigationPartials.approver')--}}
-{{--                @endif--}}
-
-{{--                @if(Auth::user()->finance)--}}
-{{--                    @include('shared.navigationPartials.finance')--}}
-{{--                @endif--}}
-
-{{--                @if(Auth::user()->admin)--}}
-{{--                    @include('shared.navigationPartials.approver')--}}
-{{--                    @include('shared.navigationPartials.finance')--}}
-{{--                @endif--}}
-{{--                <li>--}}
-{{--                    <a href="/logout">--}}
-{{--                        <i class="fas fa-sign-out-alt"></i>--}}
-{{--                    </a>--}}
-{{--                </li>--}}
-{{--            @endauth--}}
-{{--        </ul>--}}
-{{--        <br>--}}
-{{--        <br>--}}
-{{--    </div>--}}
-{{--</nav>--}}
--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,5 +1,6 @@
-@extends('layouts.template')
-@section('title','My Expense Profile')
+@extends('layouts.templateExternal')
+@section('title','My Expense Login')
+@include('shared.navigation')
 @section('main')
     <div class="container-fluid">
         <div class="row justify-content-center">
@@ -142,3 +143,75 @@
         </div>
     </div>
 @endsection
+{{--@extends('layouts.templateExternal')--}}
+{{--@section('title','My Expense Login')--}}
+{{--@section('image')--}}
+{{--    <div class="row mx-auto my-auto">--}}
+{{--        <img src="/assets/logo/MyExpenseLogo.png" class="loginLogo" alt="My expense Logo">--}}
+{{--    </div>--}}
+{{--@endsection--}}
+{{--@section('main')--}}
+{{--    <div class=" row pt-3 pr-3 float-right">--}}
+{{--        <a href="/register">--}}
+{{--            <i class="fas fa-user-plus registericon"></i>--}}
+{{--        </a>--}}
+{{--    </div>--}}
+{{--    <div class=" row login d-flex align-items-center py-5">--}}
+{{--        <div class="container">--}}
+{{--            <div class="row">--}}
+{{--                <div class="col-lg-10 col-xl-7 mx-auto pb-5">--}}
+{{--                    <div class="row justify-content-center pb-5">--}}
+{{--                        <img src="/assets/logo/MyExpenseLogo.png" class="mobilelogo d-md-none" alt="My expense Logo">--}}
+{{--                    </div>--}}
+{{--                    <h3 class="display-4 mb-5">My Expense</h3>--}}
+{{--                    <form method="POST" action="{{ route('login') }}">--}}
+{{--                        @csrf--}}
+{{--                        <div class="form-group mb-3">--}}
+{{--                            <input id="email" type="email"--}}
+{{--                                   class="rounded-pill border-0 shadow-sm px-4 form-control @error('email') is-invalid @enderror"--}}
+{{--                                   name="email" placeholder="Email-adres"--}}
+{{--                                   value="{{ old('email') }}" required autocomplete="email" autofocus>--}}
+{{--                            @error('email')--}}
+{{--                            <span class="invalid-feedback" role="alert">--}}
+{{--                                            <strong>{{ $message }}</strong>--}}
+{{--                                        </span>--}}
+{{--                            @enderror--}}
+{{--                        </div>--}}
+{{--                        <div class="form-group mb-3">--}}
+{{--                            <input id="password" type="password"--}}
+{{--                                   class=" rounded-pill border-0 shadow-sm px-4 form-control @error('password') is-invalid @enderror"--}}
+{{--                                   name="password" placeholder="Paswoord"--}}
+{{--                                   required autocomplete="current-password">--}}
+
+{{--                            @error('password')--}}
+{{--                            <span class="invalid-feedback" role="alert">--}}
+{{--                                            <strong>{{ $message }}</strong>--}}
+{{--                                        </span>--}}
+{{--                            @enderror--}}
+{{--                        </div>--}}
+{{--                        <div class="form-group ">--}}
+{{--                            <div class="form-check pl-5">--}}
+{{--                                <input class="form-check-input" type="checkbox" name="remember"--}}
+{{--                                       id="remember" {{ old('remember') ? 'checked' : '' }}>--}}
+{{--                                <label class="form-check-label small" for="remember">--}}
+{{--                                    {{ __('Onthouden') }}--}}
+{{--                                </label>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                        <div class="form-group  mt-5">--}}
+{{--                            <button type="submit"--}}
+{{--                                    class="btn btn-dark btn-block text-uppercase mb-2 rounded-pill shadow-sm">--}}
+{{--                                {{ __('Login') }}--}}
+{{--                            </button>--}}
+{{--                            @if (Route::has('password.request'))--}}
+{{--                                <a class="small pl-4" href="{{ route('password.request') }}">--}}
+{{--                                    {{ __('Oops...vergeten? Klik hier') }}--}}
+{{--                                </a>--}}
+{{--                            @endif--}}
+{{--                        </div>--}}
+{{--                    </form>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--@endsection--}}
