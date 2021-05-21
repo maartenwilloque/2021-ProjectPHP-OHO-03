369791315ba567e4b14f9fef095e64150b43a5b3
maartenwilloque <r0237151@student.thomasmore.be>
2021-05-21 16:23:00 +0200

    newseeder

--- database/migrations/2021_04_30_141600_create_types_table.php
+++ database/migrations/2021_04_30_141600_create_types_table.php
@@ -16,31 +16,29 @@ class CreateTypesTable extends Migration
         Schema::create('types', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->float('value',6,2)->nullable();
+            $table->float('value')->nullable();
             $table->timestamps();
         });
         //Insert Types
         DB::table('types')->insert(
             [
                 [
-                    'name' => 'Algemeen'
+                    'name' => 'Algemeen',
+                    'value'=> 1
                 ],
                 [
                     'name' => 'Algemeen_Laptop',
-                    'value'=> '800'
+                    'value'=> 800
 
                 ],
                 [
                     'name' => 'Vervoer_KM',
-                    'value'=> '0.50'
+                    'value'=> 0.50
                 ],
                 [
                     'name' => 'Vervoer_Fiets',
-                    'value'=> '0.15'
+                    'value'=> 0.15
 
-                ],
-                [
-                    'name' => 'Vervoer_Openbaar'
                 ],
             ]
         );
