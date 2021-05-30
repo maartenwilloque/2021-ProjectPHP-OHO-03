97b5ea1fe0e9775c6b477ea65e4c3135ef2da8ac
maartenwilloque <r0237151@student.thomasmore.be>
2021-05-30 23:04:08 +0200

    status edit

--- resources/views/layouts/templateDetails.blade.php
+++ resources/views/layouts/templateDetails.blade.php
@@ -2,17 +2,33 @@
     <div class="col-11 offset-1">
         <div class="container">
             <div class="row">
-                <h3 class="display-4 mb-3">Details</h3>
+                <h3 class="display-4">Details</h3>
             </div>
             <div class="row">
                 <div class="col-12">
-                    <h4 class>Algemeen</h4>
-                    <div class="row mt-2">
+                    <div class="row">
+                        <div class="col-6">
+                            <h4 class="mt-3">Algemeen</h4>
+                        </div>
+                        <div class="col-6 mt-2 text-right">
+                            <i class="fas fa-edit btn statusedit" style="color: orangered !important; font-size: 20px;" data-toggle="modal" data-target="#editExpensemodal"></i>
+                        </div>
+                    </div>
+                    <div class="row">
                         <div class="col-12 border detailmainbox "> @yield('detailmain')</div>
                     </div>
-                    <h4 class="mt-3">Onkosten</h4>
+                    <div class="row">
+                        <div class="col-6">
+                            <h4 class="mt-3">Onkosten</h4>
+                        </div>
+                        <div class="col-6 mt-2 text-right">
+                            <i class="far fa-plus-square btn btn-create statusedit" style="color: orangered !important; font-size: 20px;" data-toggle="modal" data-target="#createExpenselinemodal"
+                               data-id="{{$expense->id}}"></i>
+                        </div>
+                    </div>
                     <div class="row ">
-                        <div class="col-12 border detailmainbox " style="min-height:280px "> @yield('detailexpenses')</div>
+                        <div class="col-12 border detailmainbox "
+                             style="min-height:280px "> @yield('detailexpenses')</div>
                     </div>
                     <div class="row mt-3">
                         <div class="col-12"> @yield('detailsubmit')</div>
